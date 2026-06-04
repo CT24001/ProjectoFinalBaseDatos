c30a2bd5a0196589cda02333520fcd1ae9201e3e
Jesus Barahona <jesusbarahona@MacBook-Air-de-Jesus-2.local>
2026-06-03 21:12:56 -0600

    Se modifico el archivo NUMERAL6/Script_seguridad_usuario_roles

--- NUMERAL6/Script_seguridad_usuario_roles.sql
+++ NUMERAL6/Script_seguridad_usuario_roles.sql
@@ -1,27 +1,44 @@
+-- OBLIGATORIO: Asegurar que se ejecute dentro de la PDB del proyecto
+ALTER SESSION SET CONTAINER = FREEPDB1;
+
+-- LIMPIEZA PREVIA (Opcional, pero evita que falle si ya existen los usuarios)
+DROP USER usr_lectura CASCADE;
+DROP USER usr_admin CASCADE;
+
+-- 2. TUS LÍNEAS ORIGINALES DE CREACIÓN Y CONEXIÓN
 CREATE USER usr_lectura IDENTIFIED BY "UesLectura2026";
 CREATE USER usr_admin IDENTIFIED BY "UesAdmin2026";
 
+ALTER USER usr_lectura DEFAULT TABLESPACE USERS; -- Agregado para asegurar orden
 ALTER USER usr_lectura QUOTA UNLIMITED ON USERS;
+ALTER USER usr_admin DEFAULT TABLESPACE USERS;   -- Agregado para asegurar orden
 ALTER USER usr_admin QUOTA UNLIMITED ON USERS;
 
 GRANT CREATE SESSION TO usr_lectura;
 GRANT CREATE SESSION TO usr_admin;
 
--- usr_lectura: Solo SELECT de negocio
-GRANT SELECT ON Region TO usr_lectura;
-GRANT SELECT ON Pais TO usr_lectura;
-GRANT SELECT ON Localizacion TO usr_lectura;
-GRANT SELECT ON Puesto TO usr_lectura;
-GRANT SELECT ON Departamento TO usr_lectura;
-GRANT SELECT ON Empleado TO usr_lectura;
-GRANT SELECT ON Historial_Puesto TO usr_lectura;
-GRANT SELECT ON Proyecto TO usr_lectura;
-GRANT SELECT ON Asignacion TO usr_lectura;
+-- usr_lectura: Solo SELECT de negocio (CON EL PREFIJO CORRECTO)
 
--- usr_admin: EXECUTE de lógica y DML sobre auditoría/logs
-GRANT EXECUTE ON sp_resumen_periodo TO usr_admin;
-GRANT EXECUTE ON sp_top_elementos TO usr_admin;
-GRANT EXECUTE ON sp_indicadores_categoria TO usr_admin;
-GRANT EXECUTE ON sp_alertas_negocio TO usr_admin;
-GRANT SELECT, INSERT, UPDATE, DELETE ON AUDITORIA_EMPLEADOS TO usr_admin;
-GRANT SELECT, INSERT, UPDATE, DELETE ON LOG_ERRORES TO usr_admin;
+GRANT SELECT ON ADMINPROYECTO.Region TO usr_lectura;
+GRANT SELECT ON ADMINPROYECTO.Pais TO usr_lectura;
+GRANT SELECT ON ADMINPROYECTO.Localizacion TO usr_lectura;
+GRANT SELECT ON ADMINPROYECTO.Puesto TO usr_lectura;
+GRANT SELECT ON ADMINPROYECTO.Departamento TO usr_lectura;
+GRANT SELECT ON ADMINPROYECTO.Empleado TO usr_lectura;
+GRANT SELECT ON ADMINPROYECTO.Historial_Puesto TO usr_lectura;
+GRANT SELECT ON ADMINPROYECTO.Proyecto TO usr_lectura;
+GRANT SELECT ON ADMINPROYECTO.Asignacion TO usr_lectura;
+
+--AGREGADO: Permisos para las vistas 
+GRANT SELECT ON ADMINPROYECTO.vw_resumen_proyectos TO usr_lectura;
+GRANT SELECT ON ADMINPROYECTO.vw_detalle_empleados TO usr_lectura;
+
+-- usr_admin: EXECUTE de lógica y DML sobre auditoría/logs (CON PREFIJO)
+
+GRANT EXECUTE ON ADMINPROYECTO.sp_resumen_periodo TO usr_admin;
+GRANT EXECUTE ON ADMINPROYECTO.sp_top_elementos TO usr_admin;
+GRANT EXECUTE ON ADMINPROYECTO.sp_indicadores_categoria TO usr_admin;
+GRANT EXECUTE ON ADMINPROYECTO.sp_alertas_negocio TO usr_admin;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON ADMINPROYECTO.AUDITORIA_EMPLEADOS TO usr_admin;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ADMINPROYECTO.LOG_ERRORES TO usr_admin;
